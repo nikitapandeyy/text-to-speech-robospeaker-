ad5434247027a79d43e08eb3272ae8cd408590fe
NIKITA PANDEY <113332472+nikitapandeyy@users.noreply.github.com>
2023-05-17 08:50:16 +0530

    Create text_to_speech.py

---
+++ text_to_speech.py
@@ -0,0 +1,11 @@
+import pyttsx3
+if __name__ == '__main__':
+    print("Welcome to robo speaker ")
+    engine= pyttsx3.init()
+
+    while  True:
+        x = input("what you want me to speak?")
+        if x=="q":
+            break
+        engine.say(x)
+        engine.runAndWait()
